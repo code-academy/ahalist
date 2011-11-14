f517eaa5f815b747b8c17ec5324996dd6567c6e9
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-11-14 09:18:07 -0600

    updated the README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ahalist::Application.config.secret_token = 'cb5ae1f42621fd636921e04f3a4af7329b98b2b2a737618154bfba53ca7774cae38808fddaaa44c6e098c9d43b0b5269b0eebf3d2c0803e4d996208b532e7f2f'
